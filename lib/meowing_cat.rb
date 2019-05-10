0f35b2dd9e8b89537691d36e0ba6c1c8729cdf49
AJ Guiles <ashiguiles@gmail.com>
2019-05-10 02:11:32 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,13 +1,27 @@
 ## code your solution here. 
-  class Cat 
-    attr_accessor :name
+#   class Cat 
+#     attr_accessor :name
    
-   def meow
-     @name
-     puts "meow!"
-   end
- end
+#   def meow
+#     @name
+#     puts "meow!"
+#   end
+# end
  
- cat = Cat.new 
- cat.name= "Maru"
- cat.meow
+# cat = Cat.new 
+# cat.name= "Maru"
+# cat.meow
+ 
+#   #### Cat lab
+  class Cat 
+    attr_accessor :name  ##accessor 
+   
+  def meow
+    @name
+    puts "meow!" 
+  end
+end
+ 
+ cat = Cat.new   #create new instance of cat
+ cat.name= "Maru"  #give new instance a name 
+ cat.meow             # call on above method
